368bfaaaa262780226cf6ecf38b1b20c6f18875d
salomonhippie <salomonhippie@gmail.com>
2010-02-11 14:57:51 +0000

    colladaloader works again!

--- main.cpp
+++ main.cpp
@@ -11,33 +11,77 @@
 #include <Meta/Config.h>
 #include <Logging/Logger.h>
 #include <Logging/StreamLogger.h>
-#include <Core/GameEngine.h>
+#include <Core/Engine.h>
+#include <Renderers/OpenGL/RenderingView.h>
+#include <Display/Camera.h>
+#include <Devices/IKeyboard.h>
+#include <Devices/IMouse.h>
 
-// Game factory
-#include "GameFactory.h"
+// SimpleSetup
+#include <Utils/SimpleSetup.h>
+#include <Display/SDLEnvironment.h>
+#include <Display/Viewport.h>
+#include <Display/ViewingVolume.h>
 
-// name spaces that we will be using.
-// this combined with the above imports is almost the same as
-// fx. import OpenEngine.Logging.*; in Java.
+// Resources
+#include <Resources/ResourceManager.h>
+#include <Resources/ColladaResource.h>
+
+// camera tool
+#include <Utils/MouseSelection.h>
+#include <Utils/CameraTool.h>
+
 using namespace OpenEngine::Logging;
 using namespace OpenEngine::Core;
+using namespace OpenEngine::Utils;
+using namespace OpenEngine::Scene;
+using namespace OpenEngine::Display;
+using namespace OpenEngine::Renderers;
+using namespace OpenEngine::Renderers::OpenGL;
 
-/**
- * Main method for the first quarter project of CGD.
- * Corresponds to the
- *   public static void main(String args[])
- * method in Java.
- */
 int main(int argc, char** argv) {
-    // Setup logging facilities.
-    Logger::AddLogger(new StreamLogger(&std::cout));
+    int width = 800;
+    int height = 600;
+    // Create Viewport and renderingview
+    IEnvironment* env = new SDLEnvironment(width,height);
+    Viewport* vp = new Viewport(env->GetFrame());
+    IRenderingView* rv = new RenderingView(*vp);
+    // Create simple setup
+    SimpleSetup* setup = new SimpleSetup("Collada Loader", vp, env, rv);
 
-    // Print usage info.
-    logger.info << "========= Running OpenEngine Test Project =========" << logger.end;
+    string resourcedir = "projects/ColladaLoader/data/";
+    DirectoryManager::AppendPath(resourcedir);
+    ResourceManager<IModelResource>::AddPlugin(new ColladaPlugin());
+
+    ISceneNode* root = setup->GetScene();
+
+    IModelResourcePtr resource = ResourceManager<IModelResource>::Create("rock.dae");
+    resource->Load();
+    ISceneNode* node = resource->GetSceneNode();
+    resource->Unload();
+
+    root->AddNode(node);
+
+
+    // camera tool setup
+    MouseSelection* ms =
+        new MouseSelection(env->GetFrame(),
+                           setup->GetMouse(),
+                           NULL);
+    CameraTool* ct = new CameraTool(false);
+    ms->BindTool(vp, ct);
+
+    // attach the mouse tool!
+    setup->GetKeyboard().KeyEvent().Attach(*ms);
+    setup->GetMouse().MouseMovedEvent().Attach(*ms);
+    setup->GetMouse().MouseButtonEvent().Attach(*ms);
+    setup->GetRenderer().PostProcessEvent().Attach(*ms);
+
+    // FPS
+    setup->ShowFPS();
 
     // Start the engine.
-    IGameEngine& engine = GameEngine::Instance();
-    engine.Start(new GameFactory());
+    setup->GetEngine().Start();
 
     // Return when the engine stops.
     return EXIT_SUCCESS;
